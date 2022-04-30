68a3ff907154a13d2d71bb2134b2017f55b1bc53
sushma4554 <nsushmanelluri789@gmail.com>
2022-04-30 14:13:36 -0500

    completed the functonality

--- codingtest.js
+++ codingtest.js
@@ -1,3 +1,31 @@
-const getRewards = () => {
-    
+const list = [];
+const getRewards = (price) => {
+    if (price >=50 && price < 100) {
+        return price-50;
+    } else if (price >100){
+        return (2*(price-100) + 50);
+    }
+    return 0;
 }
+
+const addTransaction = (price) => {
+    const rewards = getRewards(price);
+    list.push({rewards: rewards, date: new Date()});
+}
+
+const getRewardsforThreeMonths = () => {
+    return list.filter((val) => val.date.getMonth() <= new Date().getMonth());
+}
+
+const getRewardsforThreeMonthsTotal = () => {
+    return getRewardsforThreeMonths().reduce((acc,cur) => acc +cur.rewards,0);
+}
+
+addTransaction(154);
+addTransaction(54);
+addTransaction(200);
+addTransaction(20);
+addTransaction(300);
+
+console.log(getRewardsforThreeMonths());
+console.log(getRewardsforThreeMonthsTotal());
